4ae18535039afc885d7589d86da081f75a1fd652
wisskarrou <wisskarrou@gmail.com>
2025-07-01 11:42:11 +0200

    add new way to define dataset (with one RNA file, one mol file and one interaction file rather than just one file with redundant info)

--- GerNA-Bind/data_utils/dataset.py
+++ GerNA-Bind/data_utils/dataset.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils import data
 from torch_geometric.data import Batch
 import numpy as np
-
+import json
 
 import numpy as np
 import sys
@@ -31,6 +31,25 @@ class GerNA_dataset(data.Dataset):
     def __getitem__(self, index):
         return self.RNA_repre[index], self.Mol_graph[index], self.RNA_Graph[index], self.RNA_feats[index], self.RNA_C4_coors[index], self.RNA_coors[index], self.Mol_feats[index], self.Mol_coors[index], self.LAS_input[index], self.Label_list[index]
 
+class GerNA_dataset2(data.Dataset):
+    def __init__(self, rna_dataset_path, mol_dataset_path, interaction_dataset_path):
+        with open(rna_dataset_path, 'rb') as f:
+            self.rna_data = pickle.load(f)
+        with open(mol_dataset_path, 'rb') as f:
+            self.mol_data = pickle.load(f)
+        with open(interaction_dataset_path, 'rb') as f:
+            self.interaction_data = json.load(f)
+        self.rna_id, self.RNA_repre, self.RNA_Graph, self.RNA_feats, self.RNA_C4_coors, self.RNA_coors = self.rna_data
+        self.mol_id, self.Mol_graph, self.Mol_feats, self.Mol_coors, self.LAS_input = self.mol_data
+
+    def __len__(self):
+        return len(self.RNA_repre)
+
+    def __getitem__(self, index):
+        rna_index = self.interaction_data[index]["rna"]
+        mol_index = self.interaction_data[index]["mol"]
+        return self.RNA_repre[rna_index], self.Mol_graph[mol_index], self.RNA_Graph[rna_index], self.RNA_feats[rna_index], self.RNA_C4_coors[rna_index], self.RNA_coors[rna_index], self.Mol_feats[mol_index], self.Mol_coors[mol_index], self.LAS_input[mol_index], self.interaction_data[index]["label"]
+        
 
 def custom_collate_fn(batch):
     batch_RNA_repre = []
