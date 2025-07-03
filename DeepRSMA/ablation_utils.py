47b11bba97c227d19850869a1c2ce20dab66d1fc
wisskarrou <wisskarrou@gmail.com>
2025-07-03 12:56:34 +0200

    add ablations on blind test framework

---
+++ DeepRSMA/ablation_utils.py
@@ -0,0 +1,56 @@
+import random
+import copy
+from torch.utils.data import Dataset
+from torch_geometric.data import Data
+
+
+def identity(rna_dataset, mol_dataset, seed=0):
+    return rna_dataset, mol_dataset
+
+def target_swap(rna_dataset, mol_dataset, seed=0):
+    rna_initial_mapping = [None for rna in rna_dataset]
+    rna_index = 0
+    reference_rnas = []
+    for i, rna in enumerate(rna_dataset):
+        if rna_initial_mapping[i] is None:
+            reference_x = rna.x
+            reference_rnas.append(i)
+            for j in range(i, len(rna_dataset)):
+                if rna_dataset[j].x.shape == reference_x.shape:
+                    if (rna_dataset[j].x == reference_x).all():
+                        rna_initial_mapping[j] = rna_index
+            rna_index += 1
+        
+    random.seed(seed)
+    inds = random.sample(reference_rnas, rna_index)
+    RNA_swap = [inds[rna_ind] for rna_ind in rna_initial_mapping]
+    dset_new = []
+    for i, d in enumerate(rna_dataset):
+        new_RNA = rna_dataset[RNA_swap[i]]
+        dset_new.append(
+            Data(x=new_RNA.x, edge_index=new_RNA.edge_index, y=d.y, t_id=new_RNA.T_id, e_id=new_RNA.E_id, emb=new_RNA.rna_emb, rna_len = new_RNA.rna_len)
+        )
+    return dset_new, mol_dataset
+
+
+
+def ligand_swap(rna_dataset, mol_dataset, seed=0):
+    mol_initial_mapping = [None for mol in mol_dataset]
+    mol_index = 0
+    reference_mols = []
+    for i, mol in enumerate(mol_dataset):
+        if mol_initial_mapping[i] is None:
+            reference_smiles = mol.smiles_ori
+            reference_mols.append(i)
+            for j in range(i, len(mol_dataset)):
+                if mol_dataset[j].smiles_ori == reference_smiles:
+                    mol_initial_mapping[j] = mol_index
+            mol_index += 1
+        
+    random.seed(seed)
+    inds = random.sample(reference_mols, mol_index)
+    mol_swap = [inds[mol_ind] for mol_ind in mol_initial_mapping]
+    dset_new = []
+    for i, d in enumerate(mol_dataset):
+        dset_new.append(mol_dataset[mol_swap[i]])
+    return rna_dataset, dset_new
